b78355a375fd98d352d5c6741576f72db314d101
Ben <ben.speakman@peakon.com>
2019-07-10 16:46:10 +0100

    Use jest mocks

--- src/__tests__/app.spec.ts
+++ src/__tests__/app.spec.ts
@@ -1,6 +1,5 @@
 import AWS from 'aws-sdk';
 import AWSMock from 'aws-sdk-mock';
-import sinon from 'sinon';
 import { start, yes, no, end } from '../app';
 
 AWSMock.setSDKInstance(AWS);
@@ -11,142 +10,138 @@ afterEach(() => {
 
 describe('Start round', () => {
   test('Can start a round', async () => {
-    const spy = sinon.stub().resolves({
-      Body: 'test',
-    });
-
-    AWSMock.mock('DynamoDB.DocumentClient', 'put', spy);
+    const putMock = jest.fn().mockReturnValue(Promise.resolve(true));
+    AWSMock.mock('DynamoDB.DocumentClient', 'put', putMock);
 
     expect.assertions(2);
 
     const response = await start('channelId', 'userId');
 
     expect(response.response_type).toEqual('in_channel');
-    expect(spy.callCount).toBe(1);
+    expect(putMock).toBeCalled();
   });
 
   test('Will not start a round if one exists already', async () => {
-    const spy = sinon.stub().throws('name');
-
-    AWSMock.mock('DynamoDB.DocumentClient', 'put', spy);
+    const putMock = jest.fn().mockRejectedValue(new Error('Fail'));
+    AWSMock.mock('DynamoDB.DocumentClient', 'put', putMock);
 
     expect.assertions(2);
 
     const response = await start('channelId', 'userId');
 
     expect(response.text).toEqual('There is a pub vote in this channel already');
-    expect(spy.callCount).toBe(1);
+    expect(putMock).toBeCalled();
   });
 });
 
 describe('Yes vote', () => {
   test('Can record a valid yes vote', async () => {
-    const getSpy = sinon.stub().resolves({
+    const getMock = jest.fn().mockReturnValue(Promise.resolve({
       Item: {
         users: [],
       },
-    });
+    }));
 
-    const updateSpy = sinon.stub().resolves(true);
+    const updateMock = jest.fn().mockReturnValue(Promise.resolve(true));
 
-    AWSMock.mock('DynamoDB.DocumentClient', 'get', getSpy);
-    AWSMock.mock('DynamoDB.DocumentClient', 'update', updateSpy);
+    AWSMock.mock('DynamoDB.DocumentClient', 'get', getMock);
+    AWSMock.mock('DynamoDB.DocumentClient', 'update', updateMock);
 
     expect.assertions(3);
 
     const response = await yes('channelId', 'userId', 'userName');
 
     expect(response.text).toEqual('Yass userName!');
-    expect(getSpy.callCount).toBe(1);
-    expect(updateSpy.callCount).toBe(1);
+    expect(getMock).toBeCalled();
+    expect(updateMock).toBeCalled();
   });
 
   test('Will not let a user vote yes twice', async () => {
-    const spy = sinon.stub().resolves({
+    const getMock = jest.fn().mockReturnValue(Promise.resolve({
       Item: {
         users: ['userId'],
       },
-    });
+    }));
 
-    AWSMock.mock('DynamoDB.DocumentClient', 'get', spy);
+    AWSMock.mock('DynamoDB.DocumentClient', 'get', getMock);
 
     expect.assertions(2);
 
     const response = await yes('channelId', 'userId', 'userName');
 
     expect(response.text).toEqual('You have already voted');
-    expect(spy.callCount).toBe(1);
+    expect(getMock).toBeCalled();
   });
 });
 
 describe('No vote', () => {
   test('Can record a valid no vote', async () => {
-    const getSpy = sinon.stub().resolves({
+    const getMock = jest.fn().mockReturnValue(Promise.resolve({
       Item: {
         users: ['userId'],
       },
-    });
+    }));
 
-    const updateSpy = sinon.stub().resolves(true);
+    const updateMock = jest.fn().mockReturnValue(Promise.resolve(true));
 
-    AWSMock.mock('DynamoDB.DocumentClient', 'get', getSpy);
-    AWSMock.mock('DynamoDB.DocumentClient', 'update', updateSpy);
+    AWSMock.mock('DynamoDB.DocumentClient', 'get', getMock);
+    AWSMock.mock('DynamoDB.DocumentClient', 'update', updateMock);
 
     expect.assertions(3);
 
     const response = await no('channelId', 'userId');
 
     expect(response.text).toEqual('Well you suck');
-    expect(getSpy.callCount).toBe(1);
-    expect(updateSpy.callCount).toBe(1);
+    expect(getMock).toBeCalled();
+    expect(updateMock).toBeCalled();
   });
 });
 
 describe('End round', () => {
   test('Can end a round with enough people', async () => {
-    const spy = sinon.stub().resolves({
+    const deleteMock = jest.fn().mockReturnValue(Promise.resolve({
       Attributes: {
         users: ['userId', 'userId2', 'userId3'],
       },
-    });
+    }));
 
-    AWSMock.mock('DynamoDB.DocumentClient', 'delete', spy);
+    AWSMock.mock('DynamoDB.DocumentClient', 'delete', deleteMock);
 
     expect.assertions(2);
 
     const response = await end('channelId', 'userId');
 
     expect(response.text).toEqual('Round ended with 3 people on it: <@userId>, <@userId2>, <@userId3> Assemble!');
-    expect(spy.callCount).toBe(1);
+    expect(deleteMock).toBeCalled();
   });
 
   test('Can end a round without enough people', async () => {
-    const spy = sinon.stub().resolves({
+    const deleteMock = jest.fn().mockReturnValue(Promise.resolve({
       Attributes: {
         users: ['userId'],
       },
-    });
+    }));
 
-    AWSMock.mock('DynamoDB.DocumentClient', 'delete', spy);
+    AWSMock.mock('DynamoDB.DocumentClient', 'delete', deleteMock);
 
     expect.assertions(2);
 
     const response = await end('channelId', 'userId');
 
     expect(response.text).toEqual('Not enough people are on it, try harder next time');
-    expect(spy.callCount).toBe(1);
+    expect(deleteMock).toBeCalled();
   });
 
   test('Will not let another user end the round', async () => {
-    const spy = sinon.stub().throws('name');
+    const deleteMock = jest.fn().mockRejectedValue(new Error('Fail'));
 
-    AWSMock.mock('DynamoDB.DocumentClient', 'delete', spy);
+    AWSMock.mock('DynamoDB.DocumentClient', 'delete', deleteMock);
 
     expect.assertions(2);
 
     const response = await end('channelId', 'otherUserId');
 
     expect(response.text).toEqual('You did not start this round');
-    expect(spy.callCount).toBe(1);
+    expect(deleteMock).toBeCalled();
   });
 });
